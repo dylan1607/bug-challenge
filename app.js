6e334b730a542d40a70fc9d25cdb165397d4fd2d
dylan1607 <dylan@dylans-MacBook-Pro.local>
2021-09-16 19:38:10 +0700

    update

--- app.js
+++ app.js
@@ -7,14 +7,15 @@ module.exports = (sample_text, sample_data, result_path) => {
   const data = require(sample_data);
   const sample_string = TextLoader(sample_text);
 
-  //   const processed_string = Replacer(sample_string, data);
-
   // find doublequote in processed string and replace with \\" to escape doublequote
   const processed_string = Replacer(sample_string, data).replace(/"/g, '\\"');
-  //   console.log(processed_string);
 
+  //    Another way, i think it still escape double quote
+  //   const processed_string = JSON.Stringify(Replacer(sample_string, data));
+  
   const save_path = __dirname + "/" + result_path;
 
+  // Must be remove "", if choose Json.stringify
   SaveJSON(save_path, `{ "processed": "${processed_string}" }`);
 
   const processed_json = JSONLoader(save_path);
