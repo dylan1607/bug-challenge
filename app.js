6468e0f90714e98d08b510f2b887429251f04e00
dylan1607 <dylan@dylans-MacBook-Pro.local>
2021-09-16 18:55:12 +0700

    i don't know what i'm doing

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
-const Replacer = require('./utils/Replacer')
-const SaveJSON = require('./utils/SaveJSON')
-const TextLoader = require('./utils/TextLoader')
-const JSONLoader = require('./utils/JSONLoader')
+const Replacer = require("./utils/Replacer");
+const SaveJSON = require("./utils/SaveJSON");
+const TextLoader = require("./utils/TextLoader");
+const JSONLoader = require("./utils/JSONLoader");
 
 module.exports = (sample_text, sample_data, result_path) => {
-    const data = require(sample_data)
-    const sample_string = TextLoader(sample_text)
+  const data = require(sample_data);
+  const sample_string = TextLoader(sample_text);
 
-    const processed_string = Replacer(sample_string, data)
-    const save_path = __dirname + '/' + result_path
+  //   const processed_string = Replacer(sample_string, data);
 
-    SaveJSON(save_path, `{ "processed": "${ processed_string }" }`)
+  // find doublequote in processed string and replace with \\" to escape doublequote
+  const processed_string = Replacer(sample_string, data).replace(/"/g, '\\"');
+  //   console.log(processed_string);
 
-    const processed_json = JSONLoader(save_path)
-    return processed_json
-}
+  const save_path = __dirname + "/" + result_path;
+
+  SaveJSON(save_path, `{ "processed": "${processed_string}" }`);
+
+  const processed_json = JSONLoader(save_path);
+  return processed_json;
+};
+
+// app("./data/formatted_text.txt", "./data/sample2.json", "demo.json");
